d20e81eb811c8c2b3ec8bb79222f7ed29811102a
AndreiX7 <andrei.albiller@gmail.com>
2018-11-12 12:53:37 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const html = require('html');
+const nem = require('nem-sdk');
+const app = express();
+const port = 3300;
+const mongojs = require('mongojs');
+let db = mongojs('yunyondb');
+// Routers Declaration
+const indexRouter = require('./server/routers/indexRouter');
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  req.viewModel = {
+    title: 'Test'
+  };
+  next();
+});
+
+app.set('views', path.join(__dirname, 'server/views'));
+app.set('view engine', 'html');
+
+app.use('/', indexRouter);
+
+app.listen(port, (err) => {
+  if(err) { return console.error(err); }
+  console.log(`${port}`);
+});
+
+db.on('connect', function () {
+	console.log('Database connection: OK');
+})
+
+db.on('error', function (err) {
+	console.log('database error', err)
+})
+
+db.mycollection.find(function (err, docs) {
+	console.log(docs);
+})
+
+db.users.findOne({ "username": "admin"}, (err, test) => 
+{
+  console.log(test);
+});
---
+++ server/routers/indexRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function getIndexPage(req, res) {
+  let viewModel = req.viewModel;
+  //res.render('index.html', viewModel);
+  //res.sendFile(__dirname + '/index.html');
+  res.sendFile('index.html', { root: './server/views' });
+});
+
+router.get('/login/', function getIndexPage(req, res) {
+  let viewModel = req.viewModel;
+  res.sendFile('login.html', { root: './server/views' });
+});
+
+router.post('/', function submitNotes(req, res) {
+
+});
+
+module.exports = router;
