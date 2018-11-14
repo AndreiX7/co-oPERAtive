e411fa65ce8b3d56699c9b2a0415e30d9148e9c8
AndreiX7 <andrei.albiller@gmail.com>
2018-11-14 13:25:04 +0800

    Major changes. Added 404 error default redirect page

--- index.js
+++ index.js
@@ -6,20 +6,21 @@ const html = require('html');
 const nem = require('nem-sdk');
 const app = express();
 const port = 3300;
-const mongojs = require('mongojs');
-const axios = require('axios');
-const redis = require('redis');
-const collections = ['users'];
+//const mongojs = require('mongojs');
+//const axios = require('axios');
+//const redis = require('redis');
+//const collections = ['users'];
 //var db = mongojs("127.0.0.1:27017/"+db, collections);
-let db = mongojs('yunyon.ddns.net/yunyondb', collections); // MongoDB 
+/*let db = mongojs('yunyon.ddns.net/yunyondb', collections); // MongoDB 
 var rc = redis.createClient({
   port: 6379,
   host: 'yunyon.ddns.net'
-}); // Redis Client
+}); // Redis Client*/
 
 // Routers Declaration
 const indexRouter = require('./server/routers/indexRouter');
 const loginRouter = require('./server/routers/loginRouter');
+const adminRouter = require('./server/routers/adminRouter');
 const testRouter = require('./server/routers/testRouter');
 const test2Router = require('./server/routers/test2Router');
 
@@ -41,16 +42,19 @@ app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'html');
 
 app.use('/', indexRouter);
+//app.use('/admin/dashboard', adminRouter);
 app.use('/process/login', loginRouter);
-app.use('/process/test', testRouter);
-app.use('/process/test2', test2Router);
 
 app.listen(port, (err) => {
   if(err) { return console.error(err); }
   console.log(`${port}`);
 });
 
-db.on('connect', function () {
+app.get('*', function(req, res) {
+  res.redirect('/404');
+});
+
+/*db.on('connect', function () {
 	console.log('Database connection: OK');
 })
 
@@ -69,4 +73,4 @@ db.users.findOne({ "username": "admin"}, (err, test) =>
 
 rc.on('connect', function() {
   console.log('Redis client connected');
-});
+});*/
